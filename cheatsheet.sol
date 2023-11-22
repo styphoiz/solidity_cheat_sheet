c026513d48b6f2c6b7cefcf71f10cca96347744f
Mohammed Hoosain <styphoiz@gmail.com>
2023-11-22 13:57:36 +0200

    Create cheatsheet.sol

---
+++ cheatsheet.sol
@@ -0,0 +1,17 @@
+contract Example{
+    function example1() private pure {
+        // private: call me within this contract
+        // pure: I cannot read/write to storage
+    }
+    function example2() internal view {
+        // internal: call me within this contract (+ inheritance!)
+        // view: I can read from storage, not write 
+    }
+    function example3() public payable {
+        // public: call me inside and outside this contract
+        // payable: send me some ether!
+    }
+    function example2() external {
+        // external: call me from outside this contract
+    }
+}
